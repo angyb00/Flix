6749fc764eeb1a51f54738df2a2c7a7d2267e44b
Angarag Gansukh <angaraggansukh@csu.fullerton.edu>
2022-09-07 17:56:57 -0700

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Angarag Gansukh on 9/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
