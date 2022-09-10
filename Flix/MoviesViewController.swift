22397b2e642bdbb0040e378be5131aa5cbe08478
Angarag Gansukh <angaraggansukh@csu.fullerton.edu>
2022-09-10 16:49:31 -0700

    Initial commit.

---
+++ Flix/MoviesViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Angarag Gansukh on 9/7/22.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet var tableView: UITableView!
+    var movies = [[String: Any]]()
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        // Do any additional setup after loading the view.
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { data, _, error in
+            // This will run when the network request returns
+            if let error = error {
+                print(error)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                self.movies = dataDictionary["results"] as! [[String: Any]]
+                 
+                // Call tableView functions again
+                self.tableView.reloadData()
+            }
+        }
+        task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let synopsis = movie["overview"] as! String
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)!
+        
+        // download images asynchronously
+        let task = URLSession.shared.dataTask(with: posterUrl) { data, _, error in
+            guard let data = data, let uiImage = UIImage(data: data), error == nil else {
+                print("error")
+                return
+            }
+            DispatchQueue.main.async {
+                cell.posterView.image = uiImage
+            }
+        }
+        task.resume()
+        
+        cell.titleLabel.text = title
+        cell.synopsisLabel.text = synopsis
+        return cell
+    }
+}
